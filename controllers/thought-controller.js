0855dac1125c005da529b600fa14828be527b1ee
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-02 19:52:47 -0400

    lines 25-46

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -22,3 +22,25 @@ const thoughtController = {
         });
     });
   },
+
+  removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then((deletedThought) => {
+        if (!deletedThought) {
+          return res.status(404).json({ message: "No Thought found" });
+        }
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { thoughts: params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No User Found" });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch((err) => res.json(err));
+  },
