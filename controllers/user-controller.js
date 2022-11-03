2cfe307dbc49f5470c832a2951abe53c41c51c6d
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-02 20:41:49 -0400

    line 22 -40

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,3 +19,22 @@ const userController = {
         res.status(400).json(err);
       });
   },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user with this ID" });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
