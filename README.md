970477370fd876a8a1f71ac341895d2254c7fb6d
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-02 21:57:17 -0400

    finished server.js and read me, also added package lock

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# social-network-api
+# socia-network-api
+
+## Overview: 
+
+This is an API for a social network app that uses NoSQL database. 
+
+This API creates and deletes reactions to thoughts and can also add/remove friends to a users friend list creates, updates, and deletes users and thoughts in the database 
+
+## Technologies used:
+* Express
+* JavaScript
+* MongoDB
+* Mongoose
+* Node.js
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:3001/socia-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
