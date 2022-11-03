07087989b6295a2f11d8c89149f78f4d9bb8a6db
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-02 21:06:52 -0400

    startt thought.js

---
+++ models/thought.js
@@ -0,0 +1,21 @@
+const { Schema, model, Types } = require("mongoose");
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
